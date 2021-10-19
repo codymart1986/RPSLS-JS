c8d86c84747ca9919daf14abb2e2e322c499eaef
codymart1986 <codymart1986@hotmail.com>
2021-10-19 15:26:21 -0700

    created player class

---
+++ player.js
@@ -0,0 +1,10 @@
+class Player {
+    constructor() {
+        this.name = "";
+        this.score = 0;
+        this.choice = "";
+        this.gestures = ["Rock", "Paper", "Scissors", "Lizard", "Spock"];
+    }
+}
+
+module.exports = Player;
