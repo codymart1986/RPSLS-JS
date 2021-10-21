12e789314d86456082c20b0603bdeaeb6c629b31
codymart1986 <codymart1986@hotmail.com>
2021-10-21 14:11:45 -0700

    testing, need to fix function to play again

--- game.js
+++ game.js
@@ -1,4 +1,4 @@
-const prompt = require("prompt-sync");
+const prompt = require("prompt-sync")();
 const Human = require("./human");
 const AI = require("./ai");
 
@@ -19,7 +19,7 @@ class Game {
         console.log("Scissors cuts paper, paper covers rock, rock crushes lizard, lizard poisons Spock, Spock smashes scissors, scissors decapitates lizard, lizard eats paper, paper disproves Spock, Spock vaporizes rock, and as it always has, rock crushes scissors.");
     }
     gameType() {
-        let selection = prompt("Make a choice \n Press 1 for a game against AI \n Press 2 for a local game against a petty human");
+        let selection = prompt("Make a choice, Press 1 for a game against AI, Press 2 for a local game against a petty human");
         switch(selection){
             case "1":
                 this.computerPlayer();
@@ -43,24 +43,29 @@ class Game {
             this.playerOne.chooseGesture();
             console.log("Player two's turn");
             this.playerTwo.chooseGesture();
-
+2
             if (this.playerOne.choice === this.playerTwo.choice){
                 console.log("Draw");
-            }else if (this.playerOne.choice === "Rock" && this.playerTwo.choice === "Scissors" || this.playerTwo.choice === "Lizard"){
-                console.log("Player 1 wins!");
-                this.playerOne.score += 1;
-            }else if (this.playerOne.choice === "Paper" && this.playerTwo.choice === "Rock" || this.playerTwo.choice === "Spock"){
-                console.log("Player 1 wins!");
-                this.playerOne.score += 1;
-            }else if (this.playerOne.choice === "Scissors" && this.playerTwo.choice === "Paper" || this.playerTwo.choice === "Lizard"){
-                console.log("Player 1 wins!");
-                this.playerOne.score += 1;
-            }else if (this.playerOne.choice === "Lizard" && this.playerTwo.choice === "Spock" || this.playerTwo.choice === "Paper"){
-                console.log("Player 1 wins!");
-                this.playerOne.score += 1;
-            }else if (this.playerOne.choice === "Spock" && this.playerTwo.choice === "Rock" || this.playerTwo.choice === "Scissors"){
-                console.log("Player 1 wins!");
-                this.playerOne.score += 1;
+            }else if (this.playerOne.choice === "Rock"){
+                if (this.playerTwo.choice === "Scissors" || this.playerTwo.choice === "Lizard")
+                    console.log("Player 1 wins!");
+                    this.playerOne.score += 1;
+            }else if (this.playerOne.choice === "Paper"){
+                if (this.playerTwo.choice === "Rock" || this.playerTwo.choice === "Spock");
+                    console.log("Player 1 wins!");
+                    this.playerOne.score += 1;
+            }else if (this.playerOne.choice === "Scissors"){
+                if (this.playerTwo.choice === "Paper" || this.playerTwo.choice === "Lizard")
+                    console.log("Player 1 wins!");
+                    this.playerOne.score += 1;
+            }else if (this.playerOne.choice === "Lizard"){
+                if (this.playerTwo.choice === "Spock" || this.playerTwo.choice === "Paper")
+                    console.log("Player 1 wins!");
+                    this.playerOne.score += 1;
+            }else if (this.playerOne.choice === "Spock"){
+                if (this.playerTwo.choice === "Rock" || this.playerTwo.choice === "Scissors")
+                    console.log("Player 1 wins!");
+                    this.playerOne.score += 1;
             }else{
                 console.log("Player 2 wins!");
                 this.playerTwo.score +=1;
--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const Game = require("./game")
 
-let gameOne = new Game()
-gameOne.runMain()
+let gameOne = new Game();
+gameOne.runMain();
